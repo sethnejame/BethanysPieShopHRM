23cfabc8355a203c6cf9c2076742b57bdc673df1
Seth <sethnejame@gmail.com>
2021-02-14 20:33:46 +0100

    Adding checkbox/benefit save functionality for Benefit component

--- BethanysPieShopHRM.App/Component/BenefitSelectorBase.cs
+++ BethanysPieShopHRM.App/Component/BenefitSelectorBase.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using Microsoft.AspNetCore.Components;
 using BethanysPieShopHRM.Server.Services;
 using System.Threading.Tasks;
+using System;
 
 namespace BethanysPieShopHRM.App.Component
 {
@@ -16,9 +17,30 @@ namespace BethanysPieShopHRM.App.Component
         [Parameter]
         public EmployeeModel Employee { get; set; }
 
+        protected bool SaveButtonDisabled { get; set; } = true;
+
         protected async override Task OnInitializedAsync()
         {
             Benefits = await BenefitDataService.GetForEmployee(Employee);
+        }
+
+        public void CheckBoxChanged(ChangeEventArgs e, BenefitModel benefitModel)
+        {
+            var newValue = (bool)e.Value;
+            benefitModel.Selected = newValue;
+            SaveButtonDisabled = false;
+
+            if(newValue)
+            {
+                benefitModel.StartDate = DateTime.UtcNow;
+                benefitModel.EndDate = DateTime.UtcNow.AddYears(1);
+            }
+        }
+
+        public void SaveClick()
+        {
+            BenefitDataService.UpdateForEmployee(Employee, Benefits);
+            SaveButtonDisabled = true;
         }
     }
 }
